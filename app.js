dfe8dbea44d98b22b8ba9bc11b1e69de60e7ece6
tejas <54097365+tejxv@users.noreply.github.com>
2020-04-27 20:51:23 +0530

    js fix 3

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ function showError(error){
 
 // GET WEATHER FROM API
 function getWeather(latitude, longitude){
-    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+    let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
     console.log(api)
     fetch(api)
         .then(function(response){
